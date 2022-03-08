4a25e9a126bbe51e59f05b2c6cd655f46ec2eb67
jolson2811 <jolson2811@gmail.com>
2022-03-07 20:22:58 -0700

    added drag/drop to move columns and delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,4 +182,63 @@ $("#remove-tasks").on("click", function () {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+  },
+  deactivate: function (event) {
+  },
+  over: function (event) {
+  },
+  out: function (event) {
+  },
+  update: function (event) {
+    var tempArr = [];
 
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
